87ac11cb521939fd2888f95eb673c67334f092b5
Noah <nglin34@gmail.com>
2022-05-18 17:21:22 -0400

    github

--- Sketch.java
+++ Sketch.java
@@ -2,6 +2,18 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
+
+  int[][] intGrid = new int[ROW_COUNT][COLUMN_COUNT];
+  int CELL_WIDTH = 20;
+  int CELL_HEIGHT = 20;
+  int MARGIN = 5;
+  int ROW_COUNT = 10;
+  int COLUMN_COUNT = 10;
+  int SCREEN_HEIGHT = (ROW_COUNT * CELL_HEIGHT) + ((ROW_COUNT + 1) * MARGIN);
+  int SCREEN_WIDTH = (CELL_WIDTH * COLUMN_COUNT) + ((COLUMN_COUNT + 1) * MARGIN);
+
+
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
@@ -15,13 +27,27 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(255,255,255);
+    background(0,0,0);
+
+    intGrid[1][5] = 1;
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
+
+
+
+    for(int column = 0; column <= ROW_COUNT; column++) {
+      for(int row = 0; row <= ROW_COUNT; row++) {
+        
+        rect(MARGIN*(column + 1) + (column * CELL_WIDTH), MARGIN * (row + 1) + (row * CELL_HEIGHT), CELL_HEIGHT, CELL_WIDTH);
+        if(intGrid[row][column] == 1) {
+          fill(0, 255, 0);
+        }
+      }
+    }
 	  
   }
 
