b9792ab16afb6ff1db7f252a974cea0a4a6f8d1d
noahlin34 <nglin34@gmail.com>
2022-05-21 00:53:47 -0400

    comments

--- Sketch.java
+++ Sketch.java
@@ -1,10 +1,19 @@
 import processing.core.PApplet;
 
+
+
+/**
+ * 
+ * @author noah lin
+ * 
+ * This program allows you to change cells to green and print various information about the current state of the grid 
+ */
 public class Sketch extends PApplet {
 	
 
-  int[][] intGrid;
 
+  //declaring variables
+  int[][] intGrid;
   int intRowContinuousCount = 0;
   int intSelectedColumnCount = 0;
   int intSelectedRowCount = 0;
@@ -33,6 +42,8 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(0,0,0);
+
+    //delcaring the intGrid so that its values can be undefined
     intGrid = new int[ROW_COUNT][COLUMN_COUNT];
   }
 
@@ -44,8 +55,11 @@ public class Sketch extends PApplet {
 
 
 
+    //nested for loop to draw the grid
     for(int column = 0; column < COLUMN_COUNT; column++) {
       for(int row = 0; row < ROW_COUNT; row++) {
+
+        //if the value of that array position is 1, make it green 
         if(intGrid[row][column] == 1) {
           fill(0, 255, 0);
           rect(MARGIN*(column + 1) + (column * CELL_WIDTH), MARGIN * (row + 1) + (row * CELL_HEIGHT), CELL_HEIGHT, CELL_WIDTH);
@@ -63,11 +77,19 @@ public class Sketch extends PApplet {
   }
 
   public void mousePressed(){
+
+    //nested for loop to check every space in the grid 
     for(int row = 0; row < ROW_COUNT; row++ ) {
       for(int column = 0; column < COLUMN_COUNT; column++) {
+      
+        //if the distance from the center of the cell is less than 10
         if(dist(MARGIN*(column + 1) + (column * CELL_WIDTH) + 10, MARGIN * (row + 1) + (row * CELL_HEIGHT) + 10, mouseX, mouseY) < 10 ) {
+          
+          //prints mouse position
           System.out.println("mouse coordinates: " + mouseX + ", " + mouseY + " Grid coordinates: row: " + (row + 1) + " column: " + (column + 1));
           
+
+          //code to change the squares to green and update counters 
             intGrid[row][column] ^= 1;
             if (intGrid[row][column] == 1) {
               intSelectedCount++;
@@ -118,6 +140,7 @@ public class Sketch extends PApplet {
 
     System.out.println("Total of " + intSelectedCount + " selected");
    
+    //the following for loops and if statements are to count how many consecutive cells are selected in x row 
     for(int row = 0; row <ROW_COUNT; row++) {
       for(int column = 0; column < COLUMN_COUNT; column++) {
         if(intGrid[row][column] == 1) {
@@ -136,6 +159,7 @@ public class Sketch extends PApplet {
         }
       }
 
+    //if there are more than two continuous, print the information
     if(intRowContinuousCount > 2) {
       System.out.println("There are " + intRowContinuousCount + " continuous blocks selected on row " + (row+1));
     }
@@ -144,7 +168,7 @@ public class Sketch extends PApplet {
       intRowContinuousCount = 0;
     }
 
-  
+    //for loop to display amount of selected cells in a column 
     for(int column = 0; column < COLUMN_COUNT; column++) {
       for(int row = 0; row < ROW_COUNT; row++) {
         if(intGrid[row][column] == 1) {
@@ -155,19 +179,5 @@ public class Sketch extends PApplet {
       System.out.println("Column " + (column+1) + " has " + intSelectedColumnCount + " cells selected");
       intSelectedColumnCount = 0;
     }
-
-    
-
-
-
-
-
-
-
   }
-
-  
-
-
-
 }
